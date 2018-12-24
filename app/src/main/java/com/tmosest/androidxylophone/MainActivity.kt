3b6bad3815017aa45250d2e863e9c47b75669f71
tmosest <tmosest@gmail.com>
2018-12-24 00:29:00 -0500

    really simple project but this works so far

---
+++ app/src/main/java/com/tmosest/androidxylophone/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.tmosest.androidxylophone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.media.MediaPlayer
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+
+    private fun soundIdFromViewId(viewId : Int) : Int {
+        return when(viewId) {
+            R.id.xy_note2 -> R.raw.xy_note2
+            R.id.xy_note3 -> R.raw.xy_note3
+            R.id.xy_note4 -> R.raw.xy_note4
+            R.id.xy_note5 -> R.raw.xy_note5
+            R.id.xy_note6 -> R.raw.xy_note6
+            R.id.xy_note7 -> R.raw.xy_note7
+            else -> R.raw.xy_note1
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val noteClickListener = View.OnClickListener {
+            val mp = MediaPlayer.create(this@MainActivity, soundIdFromViewId(it.id))
+            mp.start()
+        }
+        xy_note1.setOnClickListener(noteClickListener)
+        xy_note2.setOnClickListener(noteClickListener)
+        xy_note3.setOnClickListener(noteClickListener)
+        xy_note4.setOnClickListener(noteClickListener)
+        xy_note5.setOnClickListener(noteClickListener)
+        xy_note6.setOnClickListener(noteClickListener)
+        xy_note7.setOnClickListener(noteClickListener)
+    }
+}
